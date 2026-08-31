9d0f652b3323865fce604e76926481c01c5ebbc1
Muumi <ulmer.d@outlook.com>
2026-08-31 16:34:49 +0200

    Initial commit

---
+++ src/main/java/com/tidal/frostandsilence/FrostAndSilenceClient.java
@@ -0,0 +1,10 @@
+package com.tidal.frostandsilence;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrostAndSilenceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
